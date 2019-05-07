235ee8007f21b1800c829af27bc370c394b57724
Liam Cassidy <cassidyl@oregonstate.edu>
2019-05-07 16:37:44 -0700

    Tested and verified functionality for writing geometry to meshing procedure for OpenFoam. Next, writing functionality for assigning boundary conditions

--- tests/test_extract_geometry.py
+++ tests/test_extract_geometry.py
@@ -10,9 +10,19 @@ import xlrd
 import numpy
 import tkinter as tk
 from tkinter import filedialog
+from pathlib import Path
 
 def extract_geometry(file_path):
     """Pulling Data from excel workbook"""
+    file_path = Path(file_path)
+    
+    
+    """singleSlash = "\\" #    WOW THIS IS INCREDIBLE FRUSTRATING--I think the tests folder might need to be capatilized...
+    doubleSlash = "\\\\"---THE FILEPATH NEEDS TO BE SINGLE FORWARD SLASHES FOR THE PATH FUNCTION TO WORK
+    file_path_geom = file_path.replace(singleSlash,doubleSlash)"""
+    
+    """ Going to put the conversion functionality in the parse arguments section"""
+    
     workbook = xlrd.open_workbook(file_path)
     worksheet = workbook.sheet_by_name('Outputs')
     pt1x = worksheet.cell(1,2).value
@@ -70,62 +80,55 @@ def extract_geometry(file_path):
 
 def test_extract_geometry():
     """ purpose of the test is to test that geometry can be pulled from xlsx (modern) excel versions--comparing returned values to a locked spreadsheet"""
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename(title = "Please select the Stove_test_Geometry Excel file in the /tests/ directory",filetypes=[("Excel files", "*.xlsx *.xls")])
-    if file_path == None:
-        print("file path is not defined---null. Please retry")
-    else: 
-        print("File path successfully located")
-        print(file_path)
-    return file_path
-    assert extract_geometry(file_path).pt1x == 0
-    assert extract_geometry(file_path).pt1z == 0
-    assert extract_geometry(file_path).pt1y == 0
-    assert extract_geometry(file_path).pt2x == 0.1
-    assert extract_geometry(file_path).pt2z == 0
-    assert extract_geometry(file_path).pt2y == 0
-    assert extract_geometry(file_path).pt3x == 0
-    assert extract_geometry(file_path).pt3z == 0.15
-    assert extract_geometry(file_path).pt3y == 0
-    assert extract_geometry(file_path).pt4x == 0.1
-    assert extract_geometry(file_path).pt4z == 0.15
-    assert extract_geometry(file_path).pt4y == 0
-    assert extract_geometry(file_path).pt5x == 0.1
-    assert extract_geometry(file_path).pt5z == 0.16
-    assert extract_geometry(file_path).pt5y == 0
-    assert extract_geometry(file_path).pt6x == 0
-    assert extract_geometry(file_path).pt6z == 0.16
-    assert extract_geometry(file_path).pt6y == 0
-    assert extract_geometry(file_path).pt7x == 0
-    assert extract_geometry(file_path).pt7z == 0.3
-    assert extract_geometry(file_path).pt7y == 0
-    assert extract_geometry(file_path).pt8x == 0.1
-    assert extract_geometry(file_path).pt8z == 0.3
-    assert extract_geometry(file_path).pt8y == 0
-    assert extract_geometry(file_path).pt9x == 0.17
-    assert extract_geometry(file_path).pt9z == 0.3
-    assert extract_geometry(file_path).pt9y == 0
-    assert extract_geometry(file_path).pt10x == -0.07
-    assert extract_geometry(file_path).pt10z == 0.3
-    assert extract_geometry(file_path).pt10y == 0
-    assert extract_geometry(file_path).pt11x == -0.07
-    assert extract_geometry(file_path).pt11z == 0.5
-    assert extract_geometry(file_path).pt11y == 0
-    assert extract_geometry(file_path).pt12x == -.04
-    assert extract_geometry(file_path).pt12z == 0.5
-    assert extract_geometry(file_path).pt12y == 0
-    assert extract_geometry(file_path).pt13x == 0.14
-    assert extract_geometry(file_path).pt13z == 0.5
-    assert extract_geometry(file_path).pt13y == 0
-    assert extract_geometry(file_path).pt14x == 0.17
-    assert extract_geometry(file_path).pt14z == 0.5
-    assert extract_geometry(file_path).pt14y == 0
-    assert extract_geometry(file_path).pt15x == -0.04
-    assert extract_geometry(file_path).pt15z == 0.33
-    assert extract_geometry(file_path).pt15y == 0
-    assert extract_geometry(file_path).pt16x == 0.14
-    assert extract_geometry(file_path).pt16z == 0.33
-    assert extract_geometry(file_path).pt16y == 0
+    file_path = "C:/Oregon_State/Spring_2019/Soft_dev_eng/StoveOpt/tests/Stove_test_Geometry.xlsx"
+    pt1x, pt1z, pt1y, pt2x, pt2z, pt2y, pt3x, pt3z, pt3y, pt4x, pt4z, pt4y, pt5x, pt5z, pt5y, pt6x, pt6z, pt6y, pt7x, pt7z, pt7y, pt8x, pt8z, pt8y, pt9x, pt9z, pt9y, pt10x, pt10z, pt10y, pt11x, pt11z, pt11y, pt12x, pt12z, pt12y, pt13x, pt13z, pt13y,  pt14x, pt14z, pt14y, pt15x, pt15z, pt15y, pt16x, pt16z, pt16y = extract_geometry(file_path)
+    assert pt1x == 0
+    assert pt1z == 0
+    assert pt1y == 0
+    assert pt2x == 0.1
+    assert pt2z == 0
+    assert pt2y == 0
+    assert pt3x == 0
+    assert pt3z == 0.15
+    assert pt3y == 0
+    assert pt4x == 0.1
+    assert pt4z == 0.15
+    assert pt4y == 0
+    assert pt5x == 0.1
+    assert pt5z == 0.16
+    assert pt5y == 0
+    assert pt6x == 0
+    assert pt6z == 0.16
+    assert pt6y == 0
+    assert pt7x == 0
+    assert pt7z == 0.3
+    assert pt7y == 0
+    assert pt8x == 0.1
+    assert pt8z == 0.3
+    assert pt8y == 0
+    assert pt9x == 0.17
+    assert pt9z == 0.3
+    assert pt9y == 0
+    assert pt10x == -0.07
+    assert pt10z == 0.3
+    assert pt10y == 0
+    assert pt11x == -0.07
+    assert pt11z == 0.5
+    assert pt11y == 0
+    assert pt12x == -.04
+    assert pt12z == 0.5
+    assert pt12y == 0
+    assert pt13x == 0.14
+    assert pt13z == 0.5
+    assert pt13y == 0
+    assert pt14x == 0.17
+    assert pt14z == 0.5
+    assert pt14y == 0
+    assert pt15x == -0.04
+    assert pt15z == 0.33
+    assert pt15y == 0
+    assert pt16x == 0.14
+    assert pt16z == 0.33
+    assert pt16y == 0
     
     
--- tests/test_locate_geometry.py
+++ tests/test_locate_geometry.py
@@ -4,26 +4,98 @@ Created on Tue Apr 23 10:06:08 2019
 
 @author: Lee
 """
-# Status: passing current test
 
-import tkinter as tk
-from tkinter import filedialog
+import yaml
+import sys
+import argparse
 
-expected_filename = "C:/Oregon_State/Spring_2019/Soft_dev_eng/StoveOpt/tests/Stove_test_Geometry.xlsx" # Known filename
-def test_locate_geometry():
-    """ output the file location of stove geometry as a string"""
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename(title = "Please select the Stove_test_Geometry Excel file in the /tests/ directory",filetypes=[("Excel files", "*.xlsx *.xls")])
-    if file_path == None:
-        print("file path is not defined---null. Please retry")
-    else: 
-        print("File path successfully located")
+def locate_geometry(args):
+    """ Pull file path and name from the input file (command line argument)"""
+    input_file = args.inputfile #first element of args = input file
+    with open(input_file, 'r') as f:
+        # Pull the filename and path for geometry. return file path and name for future modification
+        doc = yaml.load(f)
+        path = doc['case']['geometry_file_directory'] # pulling path
+        fname = doc['case']['geometry_file_name'] #pulling filename
+        
+        type_path = type(path) # object type of path
+        type_fname = type(fname) # object type of fname
+        
+        if type_path == str:
+            print("path is string")
+        else:
+            path = str(path) # conver to string type
+            
+        if type_fname == str:
+            print("fname is string")
+        else:
+            fname = str(fname) # conver to string type
+        
+        # if type(path) == str:
+            # concatenate the string
+        file_path = path + '\\' + fname
         print(file_path)
-    if file_path == expected_filename:
-        print("file names match--pass")
-        exp = 1 # 1 indicates the filename matches the expected --in test assert expecation = 1 (pass)
-    else:
-        exp = 0 # filenames do not match   
-    assert exp == 1  
+        #else:
+        return file_path
+    
+input_file1 = "C:\Oregon_State\Spring_2019\Soft_dev_eng\StoveOpt\tests\input_test_1.yaml" # Args formats for testing
+input_file2 = "C:\Oregon_State\Spring_2019\Soft_dev_eng\StoveOpt\tests\input_test_2.yaml" # Args formats for testing
+input_file3 = "C:\Oregon_State\Spring_2019\Soft_dev_eng\StoveOpt\tests\input_test_3.yaml" # Args formats for testing
+input_file4 = "C:\Oregon_State\Spring_2019\Soft_dev_eng\StoveOpt\tests\input_test_4.yaml" # Args formats for testing
+input_file5 = "C:\Oregon_State\Spring_2019\Soft_dev_eng\StoveOpt\tests\input_test_5.yaml" # Args formats for testing
 
+""" Need to have the arguments directly fed into the parser..."""
+
+"""# Construct the argument parse and parse the arguments
+parser = argparse.ArgumentParser(description='Stove Optimization')
+# File directory argument
+parser.add_argument('-i', '--inputfile', required=True, help='path and filename for input.yaml')
+args = parser.parse_args(sys.argv[1:])
+"""
+
+# Status: passing current test
+expected_filename = "C:/Oregon_State/Spring_2019/Soft_dev_eng/StoveOpt/tests/Stove_test_Geometry.xlsx" # Known filename
+def test_locate_geometry_1():
+    """ output the file location of stove geometry as a string"""
+    parser = argparse.ArgumentParser(description='Stove Optimization')
+    parser.add_argument(input_file1) # Varying the input file
+    args = parser.parse_args(sys.argv[1:])
+    file_path = locate_geometry(args)
+    assert file_path == expected_filename
+
+def test_locate_geometry_2():
+    """ output the file location of stove geometry as a string"""
+    parser = argparse.ArgumentParser(description='Stove Optimization')
+    parser.add_argument(input_file2) # Varying the input file
+    args = parser.parse_args(sys.argv[1:])
+    file_path = locate_geometry(args)
+    file_path = locate_geometry(args)
+    assert file_path == expected_filename
+    
+def test_locate_geometry_3():
+    """ output the file location of stove geometry as a string"""
+    parser = argparse.ArgumentParser(description='Stove Optimization')
+    parser.add_argument(input_file3) # Varying the input file
+    args = parser.parse_args(sys.argv[1:])
+    file_path = locate_geometry(args)
+    file_path = locate_geometry(args)
+    assert file_path == expected_filename
+    
+def test_locate_geometry_4():
+    """ output the file location of stove geometry as a string"""
+    parser.add_argument(input_file4) # Varying the input file
+    args = parser.parse_args(sys.argv[1:])
+    file_path = locate_geometry(args)
+    file_path = locate_geometry(args)
+    assert file_path == expected_filename
+
+def test_locate_geometry_5():
+    """ output the file location of stove geometry as a string"""
+    parser = argparse.ArgumentParser(description='Stove Optimization')
+    parser.add_argument(input_file5) # Varying the input file
+    args = parser.parse_args(sys.argv[1:])
+    file_path = locate_geometry(args)
+    file_path = locate_geometry(args)
+    assert file_path == expected_filename
+    
+    
